3cff28069e1e0766d8884e33ce552949231f1876
GeorgeNechifor <georgenechifor01@gmail.com>
2023-10-22 16:47:00 +0300

    ddsd

---
+++ components/app.js
@@ -0,0 +1,10 @@
+
+
+const getNumber = () => {
+    setTimeout(() => {
+        for(let i  =0; i <= Number;++i){
+            if(number % 2 == 0) return false;
+            else return true;
+        }
+    } , 1000)
+}
